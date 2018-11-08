161d21307a32b87e7307d3f22e94bff3430bcd26
wail1994 <capsssss@hotmail.com>
2018-11-08 19:34:33 +0300

    Add project files.

---
+++ Xamarin RecyclerView/RecycleView/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.Widget;
+using XamarinRecycleView.Adapter;
+
+namespace XamarinRecycleView
+{
+    [Activity(Label = "RecycleView", MainLauncher = true, Theme ="@style/Theme.AppCompat.Light.DarkActionBar")]
+    public class MainActivity : Activity
+    {
+      
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+        }
+    }
+}
+
